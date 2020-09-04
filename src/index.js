9f34abbcc8f4b15463f6e85c4d998020ea7f49d2
Jakub Dolata <jakubdolata@192.168.1.127>
2020-09-05 01:04:32 +0200

    add SSR configuration

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'style/main.sass';
 import App from 'views/App';
 
-ReactDOM.render(
+ReactDOM.hydrate(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
